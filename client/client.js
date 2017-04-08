876e85b6eaf431e94abc72501be75df92b43d29c
Shashank Shrivastava <sshrivastava5@sapient.com>
2017-04-08 11:36:55 +0530

    React App with command line web pack configuration to generate build.js

--- client/client.js
+++ client/client.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../components/App';
 
+render(
+	// define the encompassing component,
+	// DOM element to bind with
+	<App/>, document.getElementById('app')
+);
