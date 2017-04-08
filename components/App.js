d30f5108bebf8a3d86c7e0fb5adcfe7cc081a243
Shashank Shrivastava <sshrivastava5@sapient.com>
2017-04-08 12:17:12 +0530

    Hot-Module-Reloading is implemented bundle.js is now part of build process. No need to create it separately from command line

--- components/App.js
+++ components/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class App extends Component {
 	render() {
 		return (
-			<div> This is a React App now </div>
+			<div> This is a React App with hot module reload now </div>
 		)
 	}
 }
